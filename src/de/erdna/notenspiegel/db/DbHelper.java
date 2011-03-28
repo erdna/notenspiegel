44e4e398cad28532c73f425a60e77fce86ffa206
Andre Nitzschke <andre.nitzschke@sap.com>
2011-03-28 17:07:38 +0200

    Initial commit

---
+++ src/de/erdna/notenspiegel/db/DbHelper.java
@@ -0,0 +1,34 @@
+package de.erdna.notenspiegel.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+	// database constraints
+	private static final String DATABASE_NAME = "applicationdata";
+	private static final int DATABASE_VERSION = 1;
+	
+	// table marks
+	private static final String TABLE_MARKS = "marks";
+
+	public DbHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
