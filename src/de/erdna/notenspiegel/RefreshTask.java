c9e4e9e8f133979eecf79e54314260535517a134
erdna <erdna@r2d2>
2011-11-06 16:10:16 +0100

    get hisqis via ssl

--- src/de/erdna/notenspiegel/RefreshTask.java
+++ src/de/erdna/notenspiegel/RefreshTask.java
@@ -1,30 +1,66 @@
 package de.erdna.notenspiegel;
 
 import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.DefaultedHttpParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import de.erdna.notenspiegel.db.DbAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.util.Log;
 
 public class RefreshTask extends AsyncTask<Object, Void, Void> {
-
+	
 	private Context context;
 
 	@Override
@@ -46,7 +82,7 @@ public class RefreshTask extends AsyncTask<Object, Void, Void> {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		dbAdapter.createMark("Info", "1.7");
 
 		return null;
@@ -60,36 +96,89 @@ public class RefreshTask extends AsyncTask<Object, Void, Void> {
 	}
 
 	void readWebPage() throws ClientProtocolException, IOException {
-		HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet("http://www.google.com/");
+
+		HttpClient client = getNewHttpClient();
+		
+		HttpPost request = new HttpPost("https://wwwqis.htw-dresden.de/qisserver/rds?state=user&type=1&"
+				+ "category=auth.login&startpage=portal.vm");
+		
+		// send password and user name
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+		nameValuePairs.add(new BasicNameValuePair("password", "30484"));
+		nameValuePairs.add(new BasicNameValuePair("submit", " Ok "));
+		nameValuePairs.add(new BasicNameValuePair("username", "26356"));
+		request.setEntity(new UrlEncodedFormEntity(nameValuePairs, HTTP.UTF_8));
+		
 		HttpResponse response = client.execute(request);
+
+		System.out.println("----------------------------------------");
+		System.out.println(response.getStatusLine());
+		for (Header header : response.getAllHeaders()) {
+			System.out.println(header.toString());
+		}
+		System.out.println("----------------------------------------");
+
+		if (response != null) {
+			String ret = EntityUtils.toString(response.getEntity());
+			Log.v("response", ret);
+		}
 	}
 
-	// EXAMPLE
+	public class MySSLSocketFactory extends SSLSocketFactory {
+		SSLContext sslContext = SSLContext.getInstance("TLS");
 
-	// public DefaultHttpClient getClient() {
-	// DefaultHttpClient ret = null;
-	//
-	// // SETS UP PARAMETERS
-	// HttpParams params = new BasicHttpParams();
-	// HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-	// HttpProtocolParams.setContentCharset(params, "utf-8");
-	// params.setBooleanParameter("http.protocol.expect-continue", false);
-	//
-	// // REGISTERS SCHEMES FOR BOTH HTTP AND HTTPS
-	// SchemeRegistry registry = new SchemeRegistry();
-	// registry.register(new Scheme("http", PlainSocketFactory
-	// .getSocketFactory(), 80));
-	// final SSLSocketFactory sslSocketFactory =
-	// SSLSocketFactory.getSocketFactory();
-	// sslSocketFactory
-	// .setHostnameVerifier(SSLSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
-	// registry.register(new Scheme("https", sslSocketFactory, 443));
-	//
-	// ThreadSafeClientConnManager manager = new ThreadSafeClientConnManager(
-	// params, registry);
-	// ret = new DefaultHttpClient(manager, params);
-	// return ret;
-	// }
+		public MySSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException,
+				UnrecoverableKeyException {
+			super(truststore);
+
+			TrustManager tm = new X509TrustManager() {
+				public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
+
+				public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
+
+				public X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+			};
+
+			sslContext.init(null, new TrustManager[] { tm }, null);
+		}
+
+		@Override
+		public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
+			return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+		}
+
+		@Override
+		public Socket createSocket() throws IOException {
+			return sslContext.getSocketFactory().createSocket();
+		}
+	}
+
+	public HttpClient getNewHttpClient() {
+		try {
+			KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			trustStore.load(null, null);
+
+			SSLSocketFactory sf = new MySSLSocketFactory(trustStore);
+			sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+			HttpParams params = new BasicHttpParams();
+			HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+			HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+			SchemeRegistry registry = new SchemeRegistry();
+			registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+			registry.register(new Scheme("https", sf, 443));
+
+			ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+
+			return new DefaultHttpClient(ccm, params);
+		} catch (Exception e) {
+			return new DefaultHttpClient();
+		}
+	}
 
 }
