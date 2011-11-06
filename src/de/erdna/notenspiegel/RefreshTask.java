d33c8250a5821fe2d3c84eb45dafdd07912a9446
erdna <erdna@r2d2>
2011-11-06 23:43:03 +0100

    getting table from HttpGet and its ready to parse

--- src/de/erdna/notenspiegel/RefreshTask.java
+++ src/de/erdna/notenspiegel/RefreshTask.java
@@ -1,6 +1,8 @@
 package de.erdna.notenspiegel;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.KeyManagementException;
@@ -18,63 +20,58 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.Header;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.DefaultedHttpParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
-import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HTTP;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import de.erdna.notenspiegel.db.DbAdapter;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.AsyncTask;
+import android.text.Html;
 import android.util.Log;
+import android.util.Xml;
 
 public class RefreshTask extends AsyncTask<Object, Void, Void> {
-	
+
+	private final static String HTW_HISQIS_URL = "https://wwwqis.htw-dresden.de/qisserver/rds?state=user&type=1&"
+			+ "category=auth.login&startpage=portal.vm";
+
 	private Context context;
 
 	@Override
 	protected Void doInBackground(Object... objects) {
 
-		// get context from MainActivity
+		// get context and url from MainActivity
 		context = (Context) objects[0];
+		String url = HTW_HISQIS_URL;
 
 		// Connect to DataSevice and DB
 		DbAdapter dbAdapter = new DbAdapter(context);
 		dbAdapter.open();
 
 		try {
-			readWebPage();
+			readWebPage(url);
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -95,20 +92,19 @@ public class RefreshTask extends AsyncTask<Object, Void, Void> {
 		super.onPostExecute(result);
 	}
 
-	void readWebPage() throws ClientProtocolException, IOException {
+	void readWebPage(String url) throws ClientProtocolException, IOException {
 
 		HttpClient client = getNewHttpClient();
-		
-		HttpPost request = new HttpPost("https://wwwqis.htw-dresden.de/qisserver/rds?state=user&type=1&"
-				+ "category=auth.login&startpage=portal.vm");
-		
+
+		HttpPost request = new HttpPost(Html.fromHtml(url).toString());
+
 		// send password and user name
 		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
 		nameValuePairs.add(new BasicNameValuePair("password", "30484"));
 		nameValuePairs.add(new BasicNameValuePair("submit", " Ok "));
 		nameValuePairs.add(new BasicNameValuePair("username", "26356"));
 		request.setEntity(new UrlEncodedFormEntity(nameValuePairs, HTTP.UTF_8));
-		
+
 		HttpResponse response = client.execute(request);
 
 		System.out.println("----------------------------------------");
@@ -118,10 +114,43 @@ public class RefreshTask extends AsyncTask<Object, Void, Void> {
 		}
 		System.out.println("----------------------------------------");
 
-		if (response != null) {
-			String ret = EntityUtils.toString(response.getEntity());
-			Log.v("response", ret);
+		String urlNoten = parseMenu(response.getEntity().getContent());
+		Log.v("urlNoten", urlNoten);
+
+		HttpGet requestGet = new HttpGet(Html.fromHtml(urlNoten).toString());
+		HttpResponse responseGet = client.execute(requestGet);
+
+		System.out.println("----------------------------------------");
+		System.out.println(responseGet.getStatusLine());
+		for (Header header : responseGet.getAllHeaders()) {
+			System.out.println(header.toString());
 		}
+		System.out.println("----------------------------------------");
+
+		String url2 = parseNoten(responseGet.getEntity().getContent())[1];
+		Log.v("url2", url2);
+
+		HttpGet requestNoten = new HttpGet(Html.fromHtml(url2).toString());
+		HttpResponse responseNoten = client.execute(requestNoten);
+
+		System.out.println("----------------------------------------");
+		System.out.println(responseGet.getStatusLine());
+		for (Header header : responseGet.getAllHeaders()) {
+			System.out.println(header.toString());
+		}
+		System.out.println("----------------------------------------");
+
+		parseTab(responseNoten.getEntity().getContent());
+
+//		if (responseNoten != null) {
+//			String ret = EntityUtils.toString(responseNoten.getEntity());
+//			Log.v("responseNoten", ret);
+//		}
+
+	}
+
+	private void parseTab(InputStream content) {
+		// TODO Auto-generated method stub
 	}
 
 	public class MySSLSocketFactory extends SSLSocketFactory {
@@ -179,6 +208,97 @@ public class RefreshTask extends AsyncTask<Object, Void, Void> {
 		} catch (Exception e) {
 			return new DefaultHttpClient();
 		}
+	}
+
+	private String parseMenu(InputStream htmlPage) {
+
+		final int ASSUMED_HREF_INDEX = 1;
+
+		try {
+			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+			factory.setValidating(false);
+			factory.setFeature(Xml.FEATURE_RELAXED, true);
+			factory.setNamespaceAware(true);
+			XmlPullParser xpp = factory.newPullParser();
+
+			xpp.setInput(new InputStreamReader(htmlPage));
+			int eventType = xpp.getEventType();
+			while (eventType != XmlPullParser.END_DOCUMENT) {
+
+				if (eventType == XmlPullParser.START_TAG) {
+					if (xpp.getName().equalsIgnoreCase("a") && xpp.getAttributeCount() > 1) {
+						String attributeName = xpp.getAttributeName(ASSUMED_HREF_INDEX);
+						if (attributeName.equalsIgnoreCase("href")) {
+							String link = xpp.getAttributeValue("", "href");
+							if (link.contains("=notenspiegel")) {
+								Log.v("Link", link);
+								return link;
+							}
+						}
+					}
+				}
+
+				eventType = xpp.next();
+
+			}
+
+		} catch (XmlPullParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	private String[] parseNoten(InputStream htmlPage) {
+
+		final int ASSUMED_HREF_INDEX = 1;
+		String link[] = { "", "" };
+
+		int c = 0;
+
+		try {
+			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+			factory.setValidating(false);
+			factory.setFeature(Xml.FEATURE_RELAXED, true);
+			factory.setNamespaceAware(true);
+			XmlPullParser xpp = factory.newPullParser();
+
+			xpp.setInput(new InputStreamReader(htmlPage));
+			int eventType = xpp.getEventType();
+			while (eventType != XmlPullParser.END_DOCUMENT) {
+
+				if (eventType == XmlPullParser.START_TAG) {
+					if (xpp.getName().equalsIgnoreCase("a") && xpp.getAttributeCount() > 1) {
+						String attributeName0 = xpp.getAttributeName(0);
+						// Log.v("AttributeName0", attributeName0);
+						String attributeName1 = xpp.getAttributeName(ASSUMED_HREF_INDEX);
+						// Log.v("AttributeName1", attributeName1);
+						if (attributeName0.equalsIgnoreCase("class") && attributeName1.equalsIgnoreCase("href")) {
+							String attributeValue0 = xpp.getAttributeValue(0);
+							// Log.v("AttributeValue1", attributeValue1);
+							if (attributeValue0.equals("Konto")) {
+								link[c++] = xpp.getAttributeValue("", "href");
+								Log.v("Link", link[c - 1]);
+							}
+						}
+					}
+				}
+
+				eventType = xpp.next();
+
+			}
+
+		} catch (XmlPullParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return link;
 	}
 
 }
