48e476f3c304c510dc847fa799711bc644730cfe
erdna <erdna@r2d2>
2012-02-05 22:20:24 +0100

    added final version of average dialog

--- src/de/erdna/notenspiegel/ui/AverageGradeDialog.java
+++ src/de/erdna/notenspiegel/ui/AverageGradeDialog.java
@@ -11,11 +11,6 @@ public class AverageGradeDialog extends Dialog {
 		super(context);
 		setContentView(R.layout.dialog_average_grad);
 		setTitle(R.string.dialog_average_title);
-
-		// TextView text = (TextView) findViewById(R.id.text);
-		// text.setText("Hello, this is a custom dialog!");
-		// ImageView image = (ImageView) findViewById(R.id.image);
-		// image.setImageResource(android.R.drawable.ic_menu_myplaces);
 	}
 
 	public void setCountAll(String countAll) {
@@ -32,7 +27,7 @@ public class AverageGradeDialog extends Dialog {
 		TextView text = (TextView) findViewById(R.id.textDialogSumCredits);
 		text.setText(sumCredits);
 	}
-	
+
 	public void setAverage(String average) {
 		TextView text = (TextView) findViewById(R.id.textDialogAverage);
 		text.setText(average);
